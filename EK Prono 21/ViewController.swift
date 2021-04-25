587dc4ea35e6e2643c089b3d21e340ca88f2f3e9
Stéphane Trouvé <stephanetrouve@hotmail.com>
2021-04-25 11:36:10 +0200

    Initial Commit

---
+++ EK Prono 21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EK Prono 21
+//
+//  Created by Stéphane Trouvé on 25/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EK Prono 21Tests/EK_Prono_21Tests.swift
@@ -0,0 +1,33 @@
+//
+//  EK_Prono_21Tests.swift
+//  EK Prono 21Tests
+//
+//  Created by Stéphane Trouvé on 25/04/2021.
+//
+
+import XCTest
+@testable import EK_Prono_21
+
+class EK_Prono_21Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
